408cd340f5a7e3c4de2ef02a667a008271975105
Louis Cordier <lcordier@gmail.com>
2021-03-21 12:46:41 +0200

    Add Referer header.

--- iget.py
+++ iget.py
@@ -65,11 +65,12 @@ LOGGING_CONFIG = {
 
 # Appear like a normal browser.
 HEADERS = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Proxy-Connection": "keep-alive",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                  "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
-    "Accept-Encoding": "gzip, deflate, sdch",
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Proxy-Connection': 'keep-alive',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Referer': 'https://images.google.com/',
 }
 
 # We are only interested in these formats.
